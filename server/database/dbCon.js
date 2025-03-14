95e9ac5766317d26b71bb1dd0377175f73731e71
Md. Maruf Hossain <marufhossain123098@gmail.com>
2025-03-14 15:31:33 +0600

    Update dbCon.js

--- server/database/dbCon.js
+++ server/database/dbCon.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dtaBase = async ()=>{
       try {
-         await mongoose.connect('mongodb://localhost:27017/weather_app')
+  await mongoose.connect("mongodb+srv://marufapurbo911:34oZdfCBMCOvknMP@cluster0.v5vkx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
          console.log(`database connected successfully`);
       } catch (error) {
           console.log(error);
